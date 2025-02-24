2c14bca28ef1597a8b2cb42c70233087b5c19fb1
João Roche <jcrlima@rdads.com.br>
2025-02-24 19:15:27 -0300

    feat: add routing with react-router-dom and restructure components

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('')
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      {data.length ? (
+        data.map(item => <div key={item.id}>{item.name}</div>)
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+};
+
+export default Home;
