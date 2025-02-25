1c32b2806498fc4ea9a03628fc49e2ab3eeb82f3
João Roche <jcrlima@rdads.com.br>
2025-02-24 21:49:56 -0300

    feat: add lucide-react dependency and update App structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
-import Home from './pages/Home';
-import About from './pages/About';
+import { Header } from './components/Header';
+import Home from './pages/Home/Home';
 
 
 const App = () => {
   return (
-    <Router>
-      <Header />
+      <Router>
+        <Header />
 
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+        <div className="page__container--root">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
   );
 };
 
