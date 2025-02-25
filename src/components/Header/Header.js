9bd4908a9919b4af61b01df53feff9f1310e27ab
João Roche <jcrlima@rdads.com.br>
2025-02-24 21:50:11 -0300

    feat: refactor Header component and remove global styles

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img src="/logo.png" alt="Logo Be Talent" className='header__logo-img' />
+    </header>
+  );
+};
+
