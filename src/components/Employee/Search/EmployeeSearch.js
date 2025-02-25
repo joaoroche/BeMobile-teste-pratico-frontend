db642b0efaad6075256d09555d85be2f3096db1c
João Roche <jcrlima@rdads.com.br>
2025-02-24 21:51:05 -0300

    feat: add EmployeeSearch component with styling for search functionality

---
+++ src/components/Employee/Search/EmployeeSearch.js
@@ -0,0 +1,20 @@
+import { Search } from "lucide-react"
+
+import './EmployeeSearch.css'
+
+export const EmployeeSearch = ({ searchQuery, setSearchQuery }) => {
+
+  return (
+    <div className="employee__search-container">
+      <Search className="employee__search-icon" />
+
+      <input
+        type="text"
+        placeholder="Pesquisar"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="employee__search-input"
+      />
+    </div>
+  )
+}
